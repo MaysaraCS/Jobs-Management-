9a509a4715b7729708d1c0f343ab84933cbb93f1
Maysara <maysaracs1001@gmail.com>
2025-11-26 21:02:30 +0300

    update and delete jobs

--- src/main/java/com/embarkx/firstjobapp/job/JobController.java
+++ src/main/java/com/embarkx/firstjobapp/job/JobController.java
@@ -32,6 +32,23 @@ public class JobController {
             return new ResponseEntity<>(job, HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean deleted = jobService.deleteJobById(id);
+        if (deleted)
+            return new ResponseEntity<>("Job deleted successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id,
+                                            @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJob(id, updatedJob);
+        if (updated)
+            return new ResponseEntity<>("Job updated successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
 /*
 
